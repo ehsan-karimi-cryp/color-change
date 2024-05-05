7014aeb616d0be88c32979658e10eb0541126630
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-05-05 17:48:51 +0330

    automatic change feature added

--- script.js
+++ script.js
@@ -15,10 +15,15 @@ const textAndNumbers = [
   "e",
   "f",
 ];
+var automaticChange = true;
+var automaticChangeFunction = "change";
 var randomElement = "#";
-const colorButton = document.querySelector(".color-button");
+var colorChangeInterval;
+const colorButton1 = document.querySelector(".color-button1");
+const colorButton2 = document.querySelector(".color-button2");
 const colorText = document.querySelector(".color-text");
 const bodyElement = document.querySelector(".body-element");
+
 function changeBackgroundColor() {
   for (let i = 0; i < 6; i++) {
     let arrayElement = Math.floor(Math.random() * textAndNumbers.length);
@@ -26,7 +31,23 @@ function changeBackgroundColor() {
   }
   bodyElement.style.backgroundColor = randomElement;
   colorText.innerHTML = "Color is " + randomElement;
-  randomElement = "#"
+  randomElement = "#";
 }
 
-colorButton.addEventListener("click", changeBackgroundColor);
+colorButton1.addEventListener("click", changeBackgroundColor);
+colorButton2.addEventListener("click", () => {
+  if (automaticChange === true && automaticChangeFunction === "change") {
+    colorChangeInterval = setInterval(changeBackgroundColor, 500);
+    automaticChange = false;
+    automaticChangeFunction = "stopChange";
+    colorButton2.innerHTML = "Stop Change";
+  } else if (
+    automaticChange === false &&
+    automaticChangeFunction === "stopChange"
+  ) {
+    clearInterval(colorChangeInterval);
+    colorButton2.innerHTML = "Change Automatically";
+    automaticChange = true;
+    automaticChangeFunction = "change";
+  }
+});
